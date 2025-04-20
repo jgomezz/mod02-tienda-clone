97063e13b3ade670faee28ea71e14a5cd4dced56
jgomezz <jgomezz@gmail.com>
2025-04-20 11:31:59 -0500

    Update for upload images

--- src/main/java/pe/edu/tecsup/tienda/servlets/ProductoRegistrarServlet.java
+++ src/main/java/pe/edu/tecsup/tienda/servlets/ProductoRegistrarServlet.java
@@ -1,15 +1,18 @@
 package pe.edu.tecsup.tienda.servlets;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 import pe.edu.tecsup.tienda.entities.Categoria;
 import pe.edu.tecsup.tienda.entities.Producto;
 import pe.edu.tecsup.tienda.services.CategoriaService;
@@ -19,6 +22,10 @@ import pe.edu.tecsup.tienda.services.ProductoService;
  * Servlet implementation class ProductoRegistrarServlet
  */
 @WebServlet("/ProductoRegistrarServlet")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024,
+				maxFileSize = 1024 * 1024 * 5, 
+				maxRequestSize = 1024 * 1024 * 5 * 5)
+
 public class ProductoRegistrarServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
@@ -82,6 +89,38 @@ public class ProductoRegistrarServlet extends HttpServlet {
 			producto.setPrecio(Double.parseDouble(precio));
 			producto.setStock(Integer.parseInt(stock));
 			producto.setDescripcion(descripcion); 
+			
+			Part part = request.getPart("imagen");
+			
+			if(part.getSubmittedFileName() != null) {	
+				
+				File filepath = 
+						new File(getServletContext().getRealPath("") + File.separator + "files");
+				
+				if (!filepath.exists()) 
+					filepath.mkdir();
+					
+				String filename =
+						System.currentTimeMillis() + "-" + part.getSubmittedFileName();
+				
+				part.write(filepath + File.separator + filename);
+				
+				log.info("Imagen cargada en: " + filepath + File.separator + filename);
+				
+				producto.setImagen_nombre(filename);
+				producto.setImagen_tipo(part.getContentType());
+				producto.setImagen_tamanio(part.getSize());
+				
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			log.info(producto);
 			
 			productoService.registrar(producto);
--- src/main/webapp/WEB-INF/jsp/producto/registrar.jsp
+++ src/main/webapp/WEB-INF/jsp/producto/registrar.jsp
@@ -22,7 +22,9 @@
             
     	<div class="display-4 mb-3">Mantenimiento de Productos</div>
 
-		<form action="<%=request.getContextPath()%>/ProductoRegistrarServlet" method="post">
+		<form action="<%=request.getContextPath()%>/ProductoRegistrarServlet" 
+			  method="post"
+			  enctype="multipart/form-data" >
         
 	        <div class="card">
 	            <div class="card-header">
