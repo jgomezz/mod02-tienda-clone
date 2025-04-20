fc6acab2dde264b4b9ecabdb9ce818664d70aa1e
jgomezz <jgomezz@gmail.com>
2025-04-20 08:37:36 -0500

    Update

---
+++ src/main/java/pe/edu/tecsup/tienda/utils/ConexionBD.java
@@ -0,0 +1,23 @@
+package pe.edu.tecsup.tienda.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+	
+	public static Connection obtenerConexion() throws SQLException {
+
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost/tienda_cpe?useSSL=false", "root", "");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+}
