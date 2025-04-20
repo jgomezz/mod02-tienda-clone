a211c1494b51aa32311632e037795cbc4391f572
jgomezz <jgomezz@gmail.com>
2025-04-20 12:05:14 -0500

    update for supporting pool conexion

--- src/main/java/pe/edu/tecsup/tienda/utils/ConexionBD.java
+++ src/main/java/pe/edu/tecsup/tienda/utils/ConexionBD.java
@@ -1,14 +1,36 @@
 package pe.edu.tecsup.tienda.utils;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class ConexionBD {
 	
 	public static Connection obtenerConexion() throws SQLException {
 
 		Connection con = null;
+		
+		try {
+			
+			Context initContext = new InitialContext();
+			
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			
+			DataSource datasource = (DataSource) envContext.lookup("jdbc/tienda");
+			
+			con = datasource.getConnection();
+		
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		/*
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost/tienda_cpe?useSSL=false", "root", "");
@@ -17,6 +39,9 @@ public class ConexionBD {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		*/
+		
+		
 		return con;
 	}
 
